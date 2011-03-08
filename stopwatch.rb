06a23d0c11982517a8e4dc718fbbafb18ca0c236
Chris Doherty <chris@randomcamel.net>
2011-03-07 18:38:57 -0800

    A basic stopwatch timer object for Ruby, based on one I saw in Java. A bit of a wrapper around having to track timer variables and such yourself.

---
+++ stopwatch.rb
@@ -0,0 +1,49 @@
+=begin
+    Copyright (c) 2011, Chris Doherty <chris@randomcamel.net>
+
+    Based on Corey Goldberg's StopWatch.java (http://www.goldb.org/stopwatchjava.html).
+
+    Stopwatch.rb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at
+    your option) any later version.
+
+=end
+
+class Stopwatch
+
+  def start
+    @t1 = Time.now
+  end
+
+  def stop
+    @t2 = Time.now
+  end
+
+  def reset
+    @t1 = @t2 = nil
+  end
+
+  def get_elapsed_time
+    (@t2 - @t1).to_f
+  end
+
+  def get_elapsed_time_ms
+    ((@t2 - @t1).to_f * 1000).round
+  end
+
+  def get_elapsed_time_secs
+    @t2 - @t1
+  end
+
+  # support for individual marks as static targets (held in a hash).
+
+end
+
+s = Stopwatch.new
+s.start
+sleep 2
+s.stop
+puts s.get_elapsed_time
+puts s.get_elapsed_time_ms
+puts s.get_elapsed_time_secs
